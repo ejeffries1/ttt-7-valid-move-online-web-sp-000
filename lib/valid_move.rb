16f409ffdae0cbf2efda8d4cc52e9032576c2c19
Evan Jeffries <ejeffries14@gmail.com>
2020-02-29 07:17:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if index.between?(1,9)
     true
-  else !(index.between?(1,9))
+  elsif !(index.between?(1,9))
     false
   end
      !position_taken?(board, index)
